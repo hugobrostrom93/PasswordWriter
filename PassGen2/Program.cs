02c6cdbca2ff1828d57bc13ed02dcee1548314de
HugoBror <90088173+HugoBror@users.noreply.github.com>
2022-09-03 16:33:29 +0200

    Add project files.

---
+++ PassGen2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Please enter your password: ");
+            string password = Console.ReadLine();
+            Console.WriteLine($"your new password is \"{password}\"");
+        }
+    }
+}
